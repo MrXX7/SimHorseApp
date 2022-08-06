7708b5d768de3b3f5fbdbdc11a1cf284e5d31006
Oncu <oohancan@gmail.com>
2022-08-06 23:52:09 +0300

    Initial Commit

---
+++ SimHorseApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimHorseApp
+//
+//  Created by Oncu Can on 6.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimHorseApp/SimHorseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimHorseAppApp.swift
+//  SimHorseApp
+//
+//  Created by Oncu Can on 6.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SimHorseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
